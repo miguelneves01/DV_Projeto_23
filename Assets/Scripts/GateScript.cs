bb68bb6f07f1d88670116e76e1224f36769068bb
DaniAudeamus <danielsantos9920@hotmail.com>
2023-06-26 23:21:50 +0100

    healthbar

--- Assets/Scripts/GateScript.cs
+++ Assets/Scripts/GateScript.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GateScript : MonoBehaviour
 {
     private int health;
+    private int maxHealth;
+    [SerializeField] private Image healthBar;
 
     private void Start()
     {
         health = 100 * ExperienceSystem.Instance.CurrentLevel;
+        maxHealth = health;
+        healthBar.fillAmount = 1f;
     }
 
     public void TakeDamage(int damage){
         health -= damage;
+        healthBar.fillAmount =  (float) health / (float)maxHealth;
         Debug.Log("Damage Taken");
         if (health <= 0)
         {
